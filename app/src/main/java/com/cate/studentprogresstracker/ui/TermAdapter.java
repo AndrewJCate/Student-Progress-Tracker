e0b0bf3178b8d471820fc3563b9ae2ce11e26218
IntrACate <andyboy117@hotmail.com>
2023-01-23 09:50:35 -0800

    Add term recycler view and adapter

---
+++ app/src/main/java/com/cate/studentprogresstracker/ui/TermAdapter.java
@@ -0,0 +1,85 @@
+package com.cate.studentprogresstracker.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cate.studentprogresstracker.R;
+
+import java.util.List;
+
+import entities.Term;
+
+public class TermAdapter extends RecyclerView.Adapter<TermAdapter.TermViewHolder> {
+
+    public class TermViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView termItemView;
+
+        private TermViewHolder(View itemView) {
+            super(itemView);
+            termItemView = itemView.findViewById(R.id.termItem1);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    final Term currentTerm = mTerms.get(position);
+
+                    Intent intent = new Intent(context, TermDetail.class);
+                    intent.putExtra("id", currentTerm.getTermId());
+                    intent.putExtra("title", currentTerm.getTermTitle());
+                    intent.putExtra("startDate", currentTerm.getTermStartDate());
+                    intent.putExtra("endDate", currentTerm.getTermEndDate());
+
+                    context.startActivity(intent);
+                }
+            });
+        }
+    }
+
+    private final Context context;
+    private final LayoutInflater mInflater;
+
+    private List<Term> mTerms;
+
+    public TermAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public TermAdapter.TermViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.term_list_item, parent, false);
+        return new TermViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TermAdapter.TermViewHolder holder, int position) {
+        if(mTerms != null) {
+            Term currentTerm = mTerms.get(position);
+            String title = currentTerm.getTermTitle();
+            holder.termItemView.setText(title);
+        }
+        else {
+            holder.termItemView.setText("No term name.");
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTerms.size();
+    }
+
+    public void setTerms(List<Term> terms) {
+        mTerms = terms;
+        notifyDataSetChanged();
+    }
+
+}
--- app/src/main/java/com/cate/studentprogresstracker/ui/TermsList.java
+++ app/src/main/java/com/cate/studentprogresstracker/ui/TermsList.java
@@ -1,6 +1,7 @@
 package com.cate.studentprogresstracker.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,6 +16,8 @@ public class TermsList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_list);
+
+        RecyclerView recyclerView = findViewById(R.id.termRecyclerView);
 
         FloatingActionButton fab = findViewById(R.id.floatingActionAddTermButton);
         fab.setOnClickListener(new View.OnClickListener() {
